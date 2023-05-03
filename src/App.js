0d00682ed70832f9703b08a6794b626d381537d8
justkoi42 <justkoi42@gmail.com>
2023-05-03 23:07:33 +0530

    finish routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import Header from './components/Header';
-import Welcome from './components/Welcome';
-import Main from './components/Main';
+import Header from './components/Home/Header/Header';
+import Welcome from './components/Home/Main/Welcome';
+import Main from './components/Home/Main/Main';
+import Portfolio from './components/Portfolio/Portfolio';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 import './css/app.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <Header/>
-      <Welcome/>
-      <Main/>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path='/' element={
+            <>  
+              <Welcome />
+              <Main />
+            </>
+          } />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
