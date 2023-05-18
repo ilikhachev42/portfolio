2e8ec31076f3f5a7050344d97f4254d7257894be
justkoi42 <justkoi42@gmail.com>
2023-05-18 11:52:00 +0530

    Add skills icons to the about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,9 +13,20 @@ function About() {
       <div className="container">
         <main className="photo-container">
             <div className="big-spacer"></div>
-            <h1 className="text-center big-heading text-about">About me</h1>
-            <div className="spacer"></div>
+            <div className="big-spacer"></div>
+            <h1 className="big-heading text-about">About me</h1>
             <img src={Photo} className="awesome" />
+            <div className="p-container">
+              <p>
+                With a year of front-end development experience, I am dedicated to creating user-friendly and engaging web experiences.
+              </p>
+              <p>
+                I have a strong passion for developing the perfect application logic and constantly seek to learn the most efficient ways of utilizing JavaScript and React. I am skilled in developing fully functional front-end sections and believe that I can maximize my potential as part of a team.
+              </p>
+              <p>
+                Additionally, I am interested in web game development and am eager to take on projects using libraries such as PixiJS or Three.js.
+              </p>
+            </div>
             <div className="awesome-icons">
               <img src={HTMLSVG} className="skill-icon html-icon" />
               <img src={CSSVG} className="skill-icon css-icon" />
@@ -24,16 +35,6 @@ function About() {
               <img src={BSVG} className="skill-icon bs-icon" />
               <img src={SASSVG} className="skill-icon sass-icon" />
             </div>
-            <div className="spacer"></div>
-            <p>
-              With a year of front-end development experience, I am dedicated to creating user-friendly and engaging web experiences.
-            </p>
-            <p>
-              I have a strong passion for developing the perfect application logic and constantly seek to learn the most efficient ways of utilizing JavaScript and React. I am skilled in developing fully functional front-end sections and believe that I can maximize my potential as part of a team.
-            </p>
-            <p>
-              Additionally, I am interested in web game development and am eager to take on projects using libraries such as PixiJS or Three.js.
-            </p>
             <div className="big-spacer"></div>
         </main>
       </div>
